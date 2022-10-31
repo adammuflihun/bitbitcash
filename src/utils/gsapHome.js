c6438d166171a47aa4779a28a0feb63090d9ee38
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-10-31 12:20:17 +0700

    s

--- src/utils/gsapHome.js
+++ src/utils/gsapHome.js
@@ -1,6 +1,26 @@
 import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 export const gsapHome = () => {
   console.log(`Anjay !`);
-  gsap.to('.page-section-buying-bitcoin', { borderRadius: '12rem 12rem 0 0' });
+
+  let topTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '[gsap-element="top-header"]',
+      start: 'top end',
+      end: '+=200 end',
+      //   markers: true,
+      toggleActions: 'play none reverse none',
+      scrub: 1,
+    },
+  });
+
+  topTl.fromTo(
+    '.page-section-buying-bitcoin',
+    2.5,
+    { borderRadius: '0 0 0 0' },
+    { borderRadius: '12rem 12rem 0 0', duration: 2, delay: 0.5, ease: 'Power1.easeOut' }
+  );
 };
