2ffb9bdda231d8e79565545b0515c0d3f4043f42
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-11-02 11:37:03 +0700

    s

--- src/utils/gsapHome.js
+++ src/utils/gsapHome.js
@@ -6,6 +6,7 @@ gsap.registerPlugin(ScrollTrigger);
 export const gsapHome = () => {
   console.log(`Anjay !`);
 
+  let mm = gsap.matchMedia();
   let topTl = gsap.timeline({
     scrollTrigger: {
       trigger: '[gsap-element="top-header"]',
@@ -17,10 +18,32 @@ export const gsapHome = () => {
     },
   });
 
-  topTl.fromTo(
-    '.page-section-buying-bitcoin',
-    2.5,
-    { borderRadius: '0 0 0 0' },
-    { borderRadius: '12rem 12rem 0 0', duration: 2, delay: 0.5, ease: 'Power1.easeOut' }
-  );
+  mm.add('(min-width: 800px)', () => {
+    topTl.fromTo(
+      '.page-section-buying-bitcoin',
+      2.5,
+      { borderRadius: '0 0 0 0' },
+      { borderRadius: '12rem 12rem 0 0', duration: 2, delay: 0.5, ease: 'Power1.easeOut' }
+    );
+  });
+
+  mm.add('(max-width: 799px)', () => {
+    let topTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '[gsap-element="top-header"]',
+        start: 'top end',
+        end: '+=1000 end',
+        //   markers: true,
+        toggleActions: 'play none reverse none',
+        scrub: 1,
+      },
+    });
+
+    topTl.fromTo(
+      '.page-section-buying-bitcoin',
+      2.5,
+      { borderRadius: '0 0 0 0' },
+      { borderRadius: '4rem 4rem 0 0', duration: 2, delay: 0.5, ease: 'Power1.easeOut' }
+    );
+  });
 };
